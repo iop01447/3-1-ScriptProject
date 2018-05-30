fbf5bab053dbe9ba33af5f8c61f406ac02d0cf33
lhr1 <gpfl104@naver.com>
2018-05-30 16:12:20 +0900

    장르, 국가코드 검색 변수 추가
    장르 기본값 0, 국가코드 기본값 ""
    필터링 할 때는 이 변수 값만 바꿔주면서 하면 될듯

--- test_internet.py
+++ test_internet.py
@@ -4,13 +4,17 @@ server = "openapi.naver.com"
 client_id = "Y1ISEIMTWXqCgWUOuLsD"
 client_secret = "rTt8fYC9LA"
 
-def FindKeyword():
+inputGenre = 0      # 1~28
+inputContry = ""    # 한국 (KR), 일본 (JP), 미국 (US), 홍콩 (HK), 영국 (GB), 프랑스 (FR), 기타 (ETC)
+
+def FindKeyword(inputGenre, inputContry):
     global client_id, client_secret
 
     conn = http.client.HTTPSConnection(server)
     Keyword = str(input("Keyword : "))
     encText = urllib.parse.quote(Keyword)
-    conn.request("GET", "/v1/search/movie.xml?dispaly=10&start=1&query=" + encText,
+    conn.request("GET", "/v1/search/movie.xml?dispaly=10&start=1&display=10&query=" + encText
+                 + "&genre=" + str(inputGenre) + "&contry=" + str(inputContry),
                  None, {"X-Naver-Client-Id": client_id, "X-Naver-Client-Secret": client_secret})
     req = conn.getresponse()
     response_body = req.read()
@@ -21,25 +25,28 @@ def FindKeyword():
 
     itemElements = tree.getiterator("item")  # return list type
 
-    for item in itemElements:
-        title = item.find("title").text
-        link = item.find("link").text
-        image= item.find("image").text
-        subtitle= item.find("subtitle").text
-        pubDate= item.find("pubDate").text
-        director= item.find("director").text
-        actor= item.find("actor").text
-        userRating= item.find("userRating").text
+    return itemElements
 
-        print("=========================================")
-        print("title: ", title)
-        print("link: ", link)
-        print("image url: ", image)
-        print("sub title: ", subtitle)
-        print("제작년도: ", pubDate)
-        print("영화 감독: ", director)
-        print("출연 배우: ", actor)
-        print("유저 평점: ", userRating)
-        print("=========================================")
 
-FindKeyword()
+itemElements = FindKeyword(inputGenre, inputContry)
+for item in itemElements:
+    title = item.find("title").text
+    link = item.find("link").text
+    image= item.find("image").text
+    subtitle= item.find("subtitle").text
+    pubDate= item.find("pubDate").text
+    director= item.find("director").text
+    actor= item.find("actor").text
+    userRating= item.find("userRating").text
+
+    print("=========================================")
+    print("title: ", title)
+    print("link: ", link)
+    print("image url: ", image)
+    print("sub title: ", subtitle)
+    print("제작년도: ", pubDate)
+    print("영화 감독: ", director)
+    print("출연 배우: ", actor)
+    print("유저 평점: ", userRating)
+    print("=========================================")
+
