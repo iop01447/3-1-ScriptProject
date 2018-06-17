6ed651e3bde3e3d97e1902c82919d63f0838b63d
lhr1 <gpfl104@naver.com>
2018-06-18 01:25:45 +0900

    ㅠㅠ spam 왜 안되냐 ..

--- test3.py
+++ test3.py
@@ -11,3 +11,7 @@
 # url = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcRHTrade?serviceKey=sea100UMmw23Xycs33F1EQnumONR%2F9ElxBLzkilU9Yr1oT4TrCot8Y2p0jyuJP72x9rG9D8CN5yuEs6AS2sAiw%3D%3D&LAWD_CD=11110&DEAL_YMD=201712'
 # response = urlopen(url).read()
 # print(response)
+
+import spam
+
+spam.save("test3464546")
